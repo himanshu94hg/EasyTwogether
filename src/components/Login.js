41f27e9789d47a11cc8a63085545e2ce7c796d3e
himanshu94hg <30901740+himanshu94hg@users.noreply.github.com>
2022-12-02 23:48:15 +0530

    last updates

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,85 +1,111 @@
-import React from 'react'
-import {Box , TextField , Button} from '@mui/material'
+import React from "react";
+import { Box, TextField, Button } from "@mui/material";
 import GoogleButton from "react-google-button";
-import './Login.css';
+import "./Login.css";
 import { useNavigate } from "react-router-dom";
-
+import { useState } from "react";
 
 const Login = () => {
-
   let AfterLogin = useNavigate();
-  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+
+  function validateForm() {
+    return (email.length > 0 || phone.length >= 10) && password.length >= 8;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <>
-    <Box
-      p={3}
-      className="fssl02"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "20px",
-        alignItems: "center",
-
-      }}
-    >
-      <div className="loginrightsub">
-        <h1>Easy Twogether</h1>
-        <p style={{marginBottom:'11px',fontFamily:'Roboto'}}>Welcome to EasyTwogether</p>
-        <h2>Login to your account</h2>
-       <div className='logindiv'>
-         <div className='textfeilddiv'>
-          <TextField
-            variant="outlined"
-            type="email"
-            label="Enter Email / Phone No."
-            required
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
-
-          />
-          <TextField
-             variant="outlined"
-             label="Enter Password"
-             type="password"
-             required
-             // value={password}
-             // onChange={(e) => setPassword(e.target.value)}
-
-          />
-        </div> 
-
-        <div className='fssl01'>
-          <div style={{display:'flex'}}><input type="checkbox" value="lsRememberMe" id="rememberMe"/> <span style={{marginLeft:'5px'}}></span><label for="rememberMe">Remember me</label></div>
-          <div><p style={{color:'rgba(44, 82, 130, 1)',cursor:'pointer'}}>Forgot Password?</p></div>
-        </div>
-        
- 
-      <Button
-        variant="contained"
-        className="loginbtn"
-        onClick={() => AfterLogin("./../../Welcome")}
+      <Box
+        p={3}
+        className="fssl02"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          alignItems: "center",
+        }}
       >
-        Login
-      </Button>
-       
-        
-        <div>
-        <GoogleButton
-                style={{ width: "100%", outline: "none" }}
-                label='Or sign-in with google'
-                type="dark"
-                className='googlebtn'
-                onClick={() => AfterLogin("./Welcome")}
+        <div className="loginrightsub">
+          <h1>Easy Twogether</h1>
+          <p style={{ marginBottom: "11px", fontFamily: "Roboto" }}>
+            Welcome to EasyTwogether
+          </p>
+          <h2>Login to your account</h2>
+          <div className="logindiv" onSubmit={handleSubmit}>
+            <div className="textfeilddiv">
+              <TextField
+                variant="outlined"
+                type="email"
+                label="Enter Email / Phone No."
+                required
+                value={email || phone}
+                onChange={(e) =>
+                  setEmail(e.target.value) || setPhone(e.target.value)
+                }
               />
+              <TextField
+                variant="outlined"
+                label="Enter Password"
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="fssl01">
+              <div style={{ display: "flex" }}>
+                <input type="checkbox" value="lsRememberMe" id="rememberMe" />{" "}
+                <span style={{ marginLeft: "5px" }}></span>
+                <label for="rememberMe">Remember me</label>
+              </div>
+              <div>
+                <p style={{ color: "rgba(44, 82, 130, 1)", cursor: "pointer" }}>
+                  Forgot Password?
+                </p>
+              </div>
+            </div>
+
+            <Button
+              variant="contained"
+              className="loginbtn"
+              onClick={() => AfterLogin("./../../Welcome")}
+              disabled={!validateForm()}
+            >
+              Login
+            </Button>
+
+            <div>
+              <GoogleButton
+                style={{ width: "100%", outline: "none" }}
+                label="Or sign-in with google"
+                type="dark"
+                className="googlebtn"
+                onClick={() => AfterLogin("./Welcome")}
+                disabled
+              />
+            </div>
+
+            <p style={{ marginTop: "20px" }}>
+              Dont have an account?{" "}
+              <span
+                style={{ color: "rgba(44, 82, 130, 1)", cursor: "pointer" }}
+                onClick={() => AfterLogin("./../Signup")}
+              >
+                Join free today
+              </span>
+            </p>
+          </div>
         </div>
-
-        <p style={{marginTop:'20px'}}>Dont have an account? <span style={{color:'rgba(44, 82, 130, 1)',cursor:'pointer'}} onClick={() => AfterLogin("./../Signup")}>Join free today</span></p>
-
-      </div>
-      </div>
-    </Box>
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,132 +1,150 @@
 import React from "react";
 import pic from "./img/banner.svg";
-import signupimg from "./img/signupimg.svg"
-import './Signup.css';
-import {Box , TextField , Button} from '@mui/material'
+import signupimg from "./img/signupimg.svg";
+import "./Signup.css";
+import { Box, TextField, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-
+import { useState } from "react";
 
 const Home = () => {
   let afterSignup = useNavigate();
- 
-  
-  
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  function validateForm() {
+    return (email.length > 0 || phone.length >= 10) && password.length >= 8;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
 
   return (
-   <>
-  
-     <div className="loginBackground">
-      
-      <div className="fsss01">
-       <img src={pic} alt="" className="content_961"/> 
-             
+    <>
+      <div className="loginBackground">
+        <div className="fsss01">
+          <img src={pic} alt="" className="content_961" />
+        </div>
+
+        <div className="fsss02">
+          <Box
+            p={3}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "20px",
+              alignItems: "center",
+            }}
+          >
+            <div className="fsss02sub">
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginBottom: "4%",
+                }}
+              >
+                <img style={{ width: "25%" }} src={signupimg} alt="" />
+              </div>
+              <div className="logindiv" onSubmit={handleSubmit}>
+                <div className="fsss03">
+                  <TextField
+                    variant="outlined"
+                    type="email"
+                    label="Enter Email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <TextField
+                    variant="outlined"
+                    label="Enter Mobile No."
+                    type="mobile number"
+                    required
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                  <TextField
+                    variant="outlined"
+                    label="Enter Full Name"
+                    type="name"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <TextField
+                    variant="outlined"
+                    label="Enter Username"
+                    type="name"
+                    required
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                  />
+                  <TextField
+                    variant="outlined"
+                    label="Enter Password"
+                    type="password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <TextField
+                    variant="outlined"
+                    label="Re-Enter Password"
+                    type="password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <Button
+                  variant="contained"
+                  className="loginbtn content_961"
+                  onClick={() => afterSignup("/")}
+                  disabled={!validateForm()}
+                >
+                  Sign Up
+                </Button>
+
+                <Button
+                  variant="contained"
+                  className="loginbtn content_960"
+                  onClick={() => afterSignup("./../Login")}
+                  disabled={!validateForm()}
+                >
+                  Sign Up
+                </Button>
+
+                <p style={{ marginTop: "20px" }} className="content_961">
+                  Do you already have an account?{" "}
+                  <span
+                    style={{ color: "rgba(44, 82, 130, 1)", cursor: "pointer" }}
+                    onClick={() => afterSignup("/")}
+                  >
+                    Login
+                  </span>
+                </p>
+
+                <p style={{ marginTop: "20px" }} className="content_960">
+                  Do you already have an account?{" "}
+                  <span
+                    style={{ color: "rgba(44, 82, 130, 1)", cursor: "pointer" }}
+                    onClick={() => afterSignup("./../Login")}
+                  >
+                    Login
+                  </span>
+                </p>
+              </div>
+            </div>
+          </Box>
+        </div>
       </div>
-
-
-      <div className="fsss02">
-      <Box
-      p={3}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "20px",
-        alignItems: "center",
-
-      }}
-    >
-      <div className="fsss02sub">
-        <div style={{display:'flex', justifyContent:'center', marginBottom:'4%'}}><img style={{width:'25%'}} src={signupimg} alt="" /></div>
-       <div className='logindiv'>
-         <div className='fsss03'>
-          <TextField
-            variant="outlined"
-            type="email"
-            label="Enter Email"
-            required
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
-
-          />
-          <TextField
-             variant="outlined"
-             label="Enter Mobile No."
-             type="mobile number"
-             required
-             // value={password}
-             // onChange={(e) => setPassword(e.target.value)}
-
-          />
-          <TextField
-             variant="outlined"
-             label="Enter Full Name"
-             type="name"
-             required
-             // value={password}
-             // onChange={(e) => setPassword(e.target.value)}
-
-          />
-          <TextField
-             variant="outlined"
-             label="Enter Username"
-             type="name"
-             required
-             // value={password}
-             // onChange={(e) => setPassword(e.target.value)}
-
-          />
-          <TextField
-             variant="outlined"
-             label="Enter Password"
-             type="password"
-             required
-             // value={password}
-             // onChange={(e) => setPassword(e.target.value)}
-
-          />
-          <TextField
-             variant="outlined"
-             label="Re-Enter Password"
-             type="password"
-             required
-             // value={password}
-             // onChange={(e) => setPassword(e.target.value)}
-
-          />
-          
-        </div> 
-        <Button
-        variant="contained"
-        className="loginbtn content_961"
-        onClick={() => afterSignup("/")}
-      >
-        Sign Up
-      </Button>
-      
-      <Button
-        variant="contained"
-        className="loginbtn content_960"
-        onClick={() => afterSignup("./../Login")}
-      >
-        Sign Up
-      </Button>
-
-
-      <p style={{marginTop:'20px'}} className="content_961">Do you already have an account? <span style={{color:'rgba(44, 82, 130, 1)',cursor:'pointer'}} onClick={() => afterSignup("/")}>Login</span></p>
-
-
-      <p style={{marginTop:'20px'}} className="content_960">Do you already have an account? <span style={{color:'rgba(44, 82, 130, 1)',cursor:'pointer'}} onClick={() => afterSignup("./../Login")}>Login</span></p>
-
-       
-
-      </div>
-      </div>
-    </Box>
-      </div>
-      
-      
-    </div>
-   </>
+    </>
   );
-}
+};
 
 export default Home;
